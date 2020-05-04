98e17afdc55d8a8ccb2fe26765c017af86c1c3b6
Zugengowen <zugengowen69@gmail.com>
2020-05-05 04:31:12 +0700

    Lavender Updates

--- device.mk
+++ device.mk
@@ -24,7 +24,8 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-pe \
+    $(LOCAL_PATH)/overlay-lineage \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
