f6fb352c81a6f1cb0032b31e0a014769a6072d8f
ZGSpace <64064183+ZGSpace@users.noreply.github.com>
2020-05-21 23:58:14 +0700

    Back Clang 11

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,9 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/lavender
 TARGET_KERNEL_CONFIG := lavender-perf_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := r353983c
+TARGET_KERNEL_CLANG_VERSION := 11.0
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-11.0/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm660
