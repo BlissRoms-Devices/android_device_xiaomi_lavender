063e24fae5965b9f5f224a2bc3465495364f3ff2
faham1997 <nafidfaham08@gmail.com>
2020-09-04 19:11:23 +0600

    lavender : Use Pixel Charger Animation * dropped xioameme offline charging animation
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- device.mk
+++ device.mk
@@ -257,9 +257,7 @@ PRODUCT_PACKAGES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service \
-    chargeonlymode
-
+    android.hardware.health@2.0-service
 
 # IFAA manager
 PRODUCT_PACKAGES += \
