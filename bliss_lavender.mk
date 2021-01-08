ad543eee9efd97b9ab38018268cb971cdf110285
sohamxda7 <sensoham135@gmail.com>
2021-01-08 11:59:20 +0000

    lavender : update fingerprint to redfin's january security patch
    * as well as update PRIVATE_BUILD_DESC
    
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- bliss_lavender.mk
+++ bliss_lavender.mk
@@ -30,6 +30,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lavender" \
     PRODUCT_NAME="lavender" \
-    PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V11.0.5.0.PFGMIXM release-keys"
+    PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 V11.0.1.0.QFGMIXM release-keys"
 
-BUILD_FINGERPRINT :="xiaomi/lavender/lavender:9/QKQ1.190910.002/V11.0.1.0.QFGMIXM:user/release-keys"
+BUILD_FINGERPRINT :="google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys"
